5094eaaaa8c971f4ff07d6e1fee1ee3ce7d5bc0d
apolovnikov <apolovnikov@axiomsl.com>
2018-12-01 21:38:20 +0300

    fix: image callback

--- server/static/main.js
+++ server/static/main.js
@@ -1,7 +1,7 @@
 const socket = io()
 // const button = document.querySelector('button')
 // const inp = document.querySelector('imgPrime')
-const img = document.querySelector('img')
+// const img = document.querySelector('img')
 
 function ab2str(buf) {
     return String.fromCharCode.apply(null, new Uint8Array(buf));
@@ -10,7 +10,7 @@ function ab2str(buf) {
 socket.on('resp', (data) => {
     //console.log(data)
     //console.log(ab2str(data['data']))
-    img.src="data:image/jpeg;base64,"+ab2str(data['data'])
+    image.src="data:image/jpeg;base64,"+ab2str(data['data'])
     updateImage();
 })
 
